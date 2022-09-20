c432fa9da5b70e8f6f5221d4892b5d3db26c0928
espiacent <ch.gripp@posteo.ch>
2022-09-20 23:31:42 +0200

    finish basic game functionality

--- script.js
+++ script.js
@@ -68,22 +68,75 @@ const gamePlay = (function () {
             winmsg('x');
         } if (gameBoard.fields[0] == 'o' && gameBoard.fields[1] == 'o' && gameBoard.fields[2] == 'o') {
             winmsg('o');
+        } if (gameBoard.fields[3] == 'x' && gameBoard.fields[4] == 'x' && gameBoard.fields[5] == 'x') {
+            winmsg('x');
+        } if (gameBoard.fields[3] == 'o' && gameBoard.fields[4] == 'o' && gameBoard.fields[5] == 'o') {
+            winmsg('o');
+        } if (gameBoard.fields[6] == 'x' && gameBoard.fields[7] == 'x' && gameBoard.fields[8] == 'x') {
+            winmsg('x');
+        } if (gameBoard.fields[6] == 'o' && gameBoard.fields[7] == 'o' && gameBoard.fields[8] == 'o') {
+            winmsg('o');
+        } if (gameBoard.fields[0] == 'x' && gameBoard.fields[3] == 'x' && gameBoard.fields[6] == 'x') {
+            winmsg('x');
+        } if (gameBoard.fields[0] == 'o' && gameBoard.fields[3] == 'o' && gameBoard.fields[6] == 'o') {
+            winmsg('o');
+        } if (gameBoard.fields[1] == 'x' && gameBoard.fields[4] == 'x' && gameBoard.fields[7] == 'x') {
+            winmsg('x');
+        } if (gameBoard.fields[1] == 'o' && gameBoard.fields[4] == 'o' && gameBoard.fields[7] == 'o') {
+            winmsg('o');
+        } if (gameBoard.fields[2] == 'x' && gameBoard.fields[5] == 'x' && gameBoard.fields[8] == 'x') {
+            winmsg('x');
+        } if (gameBoard.fields[2] == 'o' && gameBoard.fields[5] == 'o' && gameBoard.fields[8] == 'o') {
+            winmsg('o');
+        } if (gameBoard.fields[0] == 'x' && gameBoard.fields[4] == 'x' && gameBoard.fields[8] == 'x') {
+            winmsg('x');
+        } if (gameBoard.fields[0] == 'o' && gameBoard.fields[4] == 'o' && gameBoard.fields[8] == 'o') {
+            winmsg('o');
+        } if (gameBoard.fields[2] == 'x' && gameBoard.fields[4] == 'x' && gameBoard.fields[6] == 'x') {
+            winmsg('x');
+        } if (gameBoard.fields[2] == 'o' && gameBoard.fields[4] == 'o' && gameBoard.fields[6] == 'o') {
+            winmsg('o');
+        } if (gameBoard.fields[0] == 'x' && gameBoard.fields[1] == 'x' && gameBoard.fields[2] == 'x') {
+            winmsg('x');
+        } else {
+            checkfull()
         }
     };
 
+    const checkfull = () => {
+        if (gameBoard.fields.every(field => field !== '')) {
+            winmsg('draw');
+        }
+    }
+
     const winmsg = (player) => {
-        // show modal
-        const modal = document.querySelector('.modal-outer');
-        const header = document.querySelector('.modal-header');
-        header.textContent = `${player} wins!`;
-        modal.classList.add('open');
-        // modal button
-        const btn = document.querySelector('.playbtn');
-        btn.addEventListener('click', () => {
+        if (player == 'draw') {
+            // show modal
             const modal = document.querySelector('.modal-outer');
-            modal.classList.remove('open');
-            gameBoard.reset();
-        })
+            const header = document.querySelector('.modal-header');
+            header.textContent = 'Round draw';
+            modal.classList.add('open');
+            // modal button
+            const btn = document.querySelector('.playbtn');
+            btn.addEventListener('click', () => {
+                const modal = document.querySelector('.modal-outer');
+                modal.classList.remove('open');
+                gameBoard.reset();
+            })
+        } else {
+            // show modal
+            const modal = document.querySelector('.modal-outer');
+            const header = document.querySelector('.modal-header');
+            header.textContent = `${player} wins!`;
+            modal.classList.add('open');
+            // modal button
+            const btn = document.querySelector('.playbtn');
+            btn.addEventListener('click', () => {
+                const modal = document.querySelector('.modal-outer');
+                modal.classList.remove('open');
+                gameBoard.reset();
+            })
+        }
     };
     return { checkwin, winmsg }
 })();
@@ -93,25 +146,3 @@ document.addEventListener('DOMContentLoaded', gameBoard.init)
 
 const board = document.querySelector('.playingboard');
 board.addEventListener('click', gameBoard.move);
-
-// const gameBoard = (function () {
-
-//     const assets = () => {
-//         const board = ['x', 'x', 'o', 'x', 'o', 'x', 'o', 'o', 'x'];
-//         const playerOne = '';
-//         const playerTwo = '';
-//         return board, playerOne, playerTwo;
-//     };
-
-//     function fillBoard() {
-//         const board = ['x', 'x', 'o', 'x', 'o', 'x', 'o', 'o', 'x'];
-//         const square = document.createElement('span');
-//         const field = document.querySelector('.playing-field');
-//         board.forEach((field) => {
-//             square.innerHTMl += field;
-//         });
-//         field.appendChild(square);
-
-//     };
-//     return { assets, fillBoard }
-// })();

a74a24600de0d26e731ad72c46f478418bbdac4c
espiacent <ch.gripp@posteo.ch>
2022-09-23 13:54:37 +0200

    finish gameplay basic logic and functionality with bugfixes

--- script.js
+++ script.js
@@ -1,58 +1,34 @@
-//GAMEBOARD UI
-const gameBoard = (function () {
-
-    let fields = ['', '', '', '', '', '', '', '', ''];
-
-    const init = () => {
-        const board = document.querySelector('.playingboard');
-        for (let i = 0; i < fields.length; i++) {
-            const square = document.createElement('div');
-            square.classList.add('playingfield');
-            square.setAttribute('id', `field-${i}`);
-            square.innerText = fields[i];
-            board.appendChild(square);
-        }
-    }
-
-    const update = () => {
-        const oldfields = document.querySelectorAll('.playingfield');
-        oldfields.forEach((oldfield) => { oldfield.remove() });
-        const board = document.querySelector('.playingboard');
-        for (let i = 0; i < fields.length; i++) {
-            const square = document.createElement('div');
-            square.classList.add('playingfield');
-            square.setAttribute('id', `field-${i}`);
-            square.innerText = fields[i];
-            board.appendChild(square);
-        }
-    };
-
-    const reset = () => {
-        window.location.reload();
-    };
-
-    return { fields, init, reset, update }
-
-})();
-
 //GAMEPLAY FUNCTIONS
 const gamePlay = (function () {
 
+    const gameover = false;
+    const player = 'player';
+
     const playermove = (e) => {
+        console.log('playermove');
+        console.log(e.target.id.at(-1));
+        gamePlay.player = 'player';
+        gamePlay.gameover = false;
         if (gameBoard.fields[e.target.id.at(-1)] == '') {
             gameBoard.fields[e.target.id.at(-1)] = 'x';
             gameBoard.update();
             const page = document.querySelector('body');
             page.style.pointerEvents = 'none'
-            checkplayerwin();
+            checkwin();
+            if (gamePlay.gameover !== true) {
+                pausing(1000).then(() => aimove());
+            }
         }
     };
 
     const pausing = (time) => {
+        console.log('pausing');
         return new Promise(resolve => setTimeout(resolve, time));
     }
 
     const aimove = () => {
+        console.log('aimove');
+        gamePlay.player = 'ai';
         let emptyfields = gameBoard.fields.map((element, index) => {
             if (element == '') {
                 return index;
@@ -63,53 +39,12 @@ const gamePlay = (function () {
         gameBoard.update();
         const page = document.querySelector('body');
         page.style.pointerEvents = 'auto'
-        checkaiwin();
+        checkwin();
     };
 
 
-    const checkplayerwin = () => {
-        if (gameBoard.fields[0] == 'x' && gameBoard.fields[1] == 'x' && gameBoard.fields[2] == 'x') {
-            winmsg('x');
-        } else if (gameBoard.fields[0] == 'o' && gameBoard.fields[1] == 'o' && gameBoard.fields[2] == 'o') {
-            winmsg('o');
-        } else if (gameBoard.fields[3] == 'x' && gameBoard.fields[4] == 'x' && gameBoard.fields[5] == 'x') {
-            winmsg('x');
-        } else if (gameBoard.fields[3] == 'o' && gameBoard.fields[4] == 'o' && gameBoard.fields[5] == 'o') {
-            winmsg('o');
-        } else if (gameBoard.fields[6] == 'x' && gameBoard.fields[7] == 'x' && gameBoard.fields[8] == 'x') {
-            winmsg('x');
-        } else if (gameBoard.fields[6] == 'o' && gameBoard.fields[7] == 'o' && gameBoard.fields[8] == 'o') {
-            winmsg('o');
-        } else if (gameBoard.fields[0] == 'x' && gameBoard.fields[3] == 'x' && gameBoard.fields[6] == 'x') {
-            winmsg('x');
-        } else if (gameBoard.fields[0] == 'o' && gameBoard.fields[3] == 'o' && gameBoard.fields[6] == 'o') {
-            winmsg('o');
-        } else if (gameBoard.fields[1] == 'x' && gameBoard.fields[4] == 'x' && gameBoard.fields[7] == 'x') {
-            winmsg('x');
-        } else if (gameBoard.fields[1] == 'o' && gameBoard.fields[4] == 'o' && gameBoard.fields[7] == 'o') {
-            winmsg('o');
-        } else if (gameBoard.fields[2] == 'x' && gameBoard.fields[5] == 'x' && gameBoard.fields[8] == 'x') {
-            winmsg('x');
-        } else if (gameBoard.fields[2] == 'o' && gameBoard.fields[5] == 'o' && gameBoard.fields[8] == 'o') {
-            winmsg('o');
-        } else if (gameBoard.fields[0] == 'x' && gameBoard.fields[4] == 'x' && gameBoard.fields[8] == 'x') {
-            winmsg('x');
-        } else if (gameBoard.fields[0] == 'o' && gameBoard.fields[4] == 'o' && gameBoard.fields[8] == 'o') {
-            winmsg('o');
-        } else if (gameBoard.fields[2] == 'x' && gameBoard.fields[4] == 'x' && gameBoard.fields[6] == 'x') {
-            winmsg('x');
-        } else if (gameBoard.fields[2] == 'o' && gameBoard.fields[4] == 'o' && gameBoard.fields[6] == 'o') {
-            winmsg('o');
-        } else if (gameBoard.fields[0] == 'x' && gameBoard.fields[1] == 'x' && gameBoard.fields[2] == 'x') {
-            winmsg('x');
-        } else if (gameBoard.fields.every(field => field !== '')) {
-            winmsg('draw');
-        } else {
-            pausing(1000).then(() => aimove());
-        }
-    };
-
-    const checkaiwin = () => {
+    const checkwin = () => {
+        console.log('checkwin');
         if (gameBoard.fields[0] == 'x' && gameBoard.fields[1] == 'x' && gameBoard.fields[2] == 'x') {
             winmsg('x');
         } else if (gameBoard.fields[0] == 'o' && gameBoard.fields[1] == 'o' && gameBoard.fields[2] == 'o') {
@@ -150,6 +85,8 @@ const gamePlay = (function () {
     };
 
     const winmsg = (player) => {
+        console.log('winmsg');
+        gamePlay.gameover = true;
         const page = document.querySelector('body');
         page.style.pointerEvents = 'auto'
         if (player == 'draw') {
@@ -173,14 +110,69 @@ const gamePlay = (function () {
             modal.classList.add('open');
             // modal button
             const btn = document.querySelector('.playbtn');
-            btn.addEventListener('click', () => {
+            btn.addEventListener('mouseup', () => {
                 const modal = document.querySelector('.modal-outer');
                 modal.classList.remove('open');
                 gameBoard.reset();
             })
         }
     };
+
+    // const minimax = (currboard, currmark) => {
+
+    // };
+
     return { playermove }
+})();
+
+//GAMEBOARD UI
+const gameBoard = (function () {
+
+    let fields = ['', '', '', '', '', '', '', '', ''];
+
+    const init = () => {
+        console.log('init');
+        const board = document.querySelector('.playingboard');
+        for (let i = 0; i < fields.length; i++) {
+            const square = document.createElement('div');
+            square.classList.add('playingfield');
+            square.setAttribute('id', `field-${i}`);
+            square.innerText = fields[i];
+            board.appendChild(square);
+        }
+    }
+
+    const update = () => {
+        console.log('update');
+        const board = document.querySelector('.playingboard');
+        board.innerHTML = '';
+        for (let i = 0; i < gameBoard.fields.length; i++) {
+            const square = document.createElement('div');
+            square.classList.add('playingfield');
+            square.setAttribute('id', `field-${i}`);
+            square.innerText = gameBoard.fields[i];
+            board.appendChild(square);
+        }
+    };
+
+    const reset = () => {
+        console.log('reset');
+        gamePlay.gameover = false;
+        gameBoard.fields = ['', '', '', '', '', '', '', '', ''];
+        fields = ['', '', '', '', '', '', '', '', ''];
+        const board = document.querySelector('.playingboard');
+        board.innerHTML = '';
+        for (let i = 0; i < fields.length; i++) {
+            const square = document.createElement('div');
+            square.classList.add('playingfield');
+            square.setAttribute('id', `field-${i}`);
+            square.innerText = fields[i];
+            board.appendChild(square);
+        }
+    };
+
+    return { fields, init, reset, update }
+
 })();
 
 //EVENT LISTENERS

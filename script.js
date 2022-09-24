37f222c887d2078ab4bf975532e41e324d71e096
espiacent <ch.gripp@posteo.ch>
2022-09-24 13:31:35 +0200

    new start

--- script.js
+++ script.js
@@ -35,13 +35,12 @@ const gamePlay = (function () {
                 return index;
             }
         }).filter(element => element >= 0);
-        // let random = gamePlay.emptyfields[Math.floor((Math.random() * gamePlay.emptyfields.length))];
-        // gameBoard.fields[random] = 'o';
-        // gameBoard.update();
-        // const page = document.querySelector('body');
-        // page.style.pointerEvents = 'auto'
+        let random = gamePlay.emptyfields[Math.floor((Math.random() * gamePlay.emptyfields.length))];
+        gameBoard.fields[random] = 'o';
+        gameBoard.update();
+        const page = document.querySelector('body');
+        page.style.pointerEvents = 'auto'
         checkwin();
-        //minmax invocation
         gameBoard.update();
     };
 
@@ -49,61 +48,41 @@ const gamePlay = (function () {
     const checkwin = () => {
         console.log('checkwin');
         if (gameBoard.fields[0] == 'x' && gameBoard.fields[1] == 'x' && gameBoard.fields[2] == 'x') {
-            // winmsg('x');
-            return true;
+            winmsg('x');
         } else if (gameBoard.fields[0] == 'o' && gameBoard.fields[1] == 'o' && gameBoard.fields[2] == 'o') {
-            // winmsg('o');
-            return true;
+            winmsg('o');
         } else if (gameBoard.fields[3] == 'x' && gameBoard.fields[4] == 'x' && gameBoard.fields[5] == 'x') {
-            // winmsg('x');
-            return true;
+            winmsg('x');
         } else if (gameBoard.fields[3] == 'o' && gameBoard.fields[4] == 'o' && gameBoard.fields[5] == 'o') {
-            // winmsg('o');
-            return true;
+            winmsg('o');
         } else if (gameBoard.fields[6] == 'x' && gameBoard.fields[7] == 'x' && gameBoard.fields[8] == 'x') {
-            // winmsg('x');
-            return true;
+            winmsg('x');
         } else if (gameBoard.fields[6] == 'o' && gameBoard.fields[7] == 'o' && gameBoard.fields[8] == 'o') {
-            // winmsg('o');
-            return true;
+            winmsg('o');
         } else if (gameBoard.fields[0] == 'x' && gameBoard.fields[3] == 'x' && gameBoard.fields[6] == 'x') {
-            // winmsg('x');
-            return true;
+            winmsg('x');
         } else if (gameBoard.fields[0] == 'o' && gameBoard.fields[3] == 'o' && gameBoard.fields[6] == 'o') {
-            // winmsg('o');
-            return true;
+            winmsg('o');
         } else if (gameBoard.fields[1] == 'x' && gameBoard.fields[4] == 'x' && gameBoard.fields[7] == 'x') {
-            // winmsg('x');
-            return true;
+            winmsg('x');
         } else if (gameBoard.fields[1] == 'o' && gameBoard.fields[4] == 'o' && gameBoard.fields[7] == 'o') {
-            // winmsg('o');
-            return true;
+            winmsg('o');
         } else if (gameBoard.fields[2] == 'x' && gameBoard.fields[5] == 'x' && gameBoard.fields[8] == 'x') {
-            // winmsg('x');
-            return true;
+            winmsg('x');
         } else if (gameBoard.fields[2] == 'o' && gameBoard.fields[5] == 'o' && gameBoard.fields[8] == 'o') {
-            // winmsg('o');
-            return true;
+            winmsg('o');
         } else if (gameBoard.fields[0] == 'x' && gameBoard.fields[4] == 'x' && gameBoard.fields[8] == 'x') {
-            // winmsg('x');
-            return true;
+            winmsg('x');
         } else if (gameBoard.fields[0] == 'o' && gameBoard.fields[4] == 'o' && gameBoard.fields[8] == 'o') {
-            // winmsg('o');
-            return true;
+            winmsg('o');
         } else if (gameBoard.fields[2] == 'x' && gameBoard.fields[4] == 'x' && gameBoard.fields[6] == 'x') {
-            // winmsg('x');
-            return true;
+            winmsg('x');
         } else if (gameBoard.fields[2] == 'o' && gameBoard.fields[4] == 'o' && gameBoard.fields[6] == 'o') {
-            // winmsg('o');
-            return true;
+            winmsg('o');
         } else if (gameBoard.fields[0] == 'x' && gameBoard.fields[1] == 'x' && gameBoard.fields[2] == 'x') {
-            // winmsg('x');
-            return true;
+            winmsg('x');
         } else if (gameBoard.fields.every(field => field !== '')) {
-            // winmsg('draw');
-            return true;
-        } else {
-            return false;
+            winmsg('draw');
         }
     };
 
@@ -139,104 +118,10 @@ const gamePlay = (function () {
                 gameBoard.reset();
             })
         }
-    };
-
-    const getBestMove = (gameBoard.fields, maximizing = true, callback = () => { }, depth = 0) => {
-    //clear nodesMap if the function is called for a new move
-    if (depth == 0) this.nodesMap.clear();
-
-    //If the board state is a terminal one, return the heuristic value
-    if (checkwin() || depth === this.maxDepth) {
-        if (board.isTerminal().winner === 'x') {
-            return 100 - depth;
-        } else if (checkwin().winner === 'o') {
-            return -100 + depth;
-        }
-        return 0;
-    }
-    if (maximizing) {
-        //Initialize best to the lowest possible value
-        let best = -100;
-        //Loop through all empty cells
-        board.getAvailableMoves().forEach(index => {
-            //Initialize a new board with a copy of our current state 
-            const child = new Board([...board.state]);
-            //Create a child node by inserting the maximizing symbol x into the current empty cell
-            child.insert('x', index);
-            //Recursively calling getBestMove this time with the new board and minimizing turn and incrementing the depth
-            const nodeValue = this.getBestMove(child, false, callback, depth + 1);
-            //Updating best value
-            best = Math.max(best, nodeValue);
-
-            //If it's the main function call, not a recursive one, map each heuristic value with it's moves indices
-            if (depth == 0) {
-                //Comma separated indices if multiple moves have the same heuristic value
-                const moves = this.nodesMap.has(nodeValue) ? `${this.nodesMap.get(nodeValue)},${index}` : index;
-                this.nodesMap.set(nodeValue, moves);
-            }
-        });
-        //If it's the main call, return the index of the best move or a random index if multiple indices have the same value
-        if (depth == 0) {
-            let returnValue;
-            if (typeof this.nodesMap.get(best) == 'string') {
-                const arr = this.nodesMap.get(best).split(',');
-                const rand = Math.floor(Math.random() * arr.length);
-                returnValue = arr[rand];
-            } else {
-                returnValue = this.nodesMap.get(best);
-            }
-            //run a callback after calculation and return the index
-            callback(returnValue);
-            return returnValue;
-        }
-        //If not main call (recursive) return the heuristic value for next calculation
-        return best;
     }
 
-    if (!maximizing) {
-        //Initialize best to the highest possible value
-        let best = 100;
-        //Loop through all empty cells
-        board.getAvailableMoves().forEach(index => {
-            //Initialize a new board with a copy of our current state 
-            const child = new Board([...board.state]);
-
-            //Create a child node by inserting the minimizing symbol o into the current empty cell
-            child.insert('o', index);
-
-            //Recursively calling getBestMove this time with the new board and maximizing turn and incrementing the depth
-            let nodeValue = this.getBestMove(child, true, callback, depth + 1);
-            //Updating best value
-            best = Math.min(best, nodeValue);
-
-            //If it's the main function call, not a recursive one, map each heuristic value with it's moves indices
-            if (depth == 0) {
-                //Comma separated indices if multiple moves have the same heuristic value
-                const moves = this.nodesMap.has(nodeValue) ? this.nodesMap.get(nodeValue) + ',' + index : index;
-                this.nodesMap.set(nodeValue, moves);
-            }
-        });
-        //If it's the main call, return the index of the best move or a random index if multiple indices have the same value
-        if (depth == 0) {
-            let returnValue;
-            if (typeof this.nodesMap.get(best) == 'string') {
-                const arr = this.nodesMap.get(best).split(',');
-                const rand = Math.floor(Math.random() * arr.length);
-                returnValue = arr[rand];
-            } else {
-                returnValue = this.nodesMap.get(best);
-            }
-            //run a callback after calculation and return the index
-            callback(returnValue);
-            return returnValue;
-        }
-        //If not main call (recursive) return the heuristic value for next calculation
-        return best;
-    }
-}
-
-return { playermove, getBestMove }
-}) ();
+    return { playermove }
+})();
 
 //GAMEBOARD UI
 const gameBoard = (function () {
